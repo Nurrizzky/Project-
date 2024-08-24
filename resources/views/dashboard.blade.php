8e02fa594f5db7c9a229ded61286da8cbf9f9df3
MuhamadAzmiN <azmi89017@gmail.com>
2024-08-24 15:09:48 +0700

    updatesore

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,27 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{-- <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
+        </h2> --}}
+        @if(!auth()->user())
+
+    <div class="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div class="flex items-center justify-center w-12 bg-yellow-400">
+            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+            </svg>
+        </div>
+
+        <div class="px-4 py-2 -mx-3">
+            <div class="mx-3">
+                <span class="font-semibold text-yellow-400 dark:text-yellow-300">Warning</span>
+                <p class="text-sm text-gray-600 dark:text-gray-200">
+                    Anda Belum login Silahkan Login Disini <a class="text-yellow-300" href="/login">Login</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    @endif
     </x-slot>
 
     {{-- <div class="py-12">
@@ -14,6 +33,9 @@
             </div>
         </div>
     </div> --}}
+
+    
+
     <div id="default-carousel" class="relative h-56 overflow-hidden md:h-96" data-carousel="static">
         <!-- Carousel wrapper -->
         <div class="relative h-full overflow-hidden rounded-lg">
@@ -74,7 +96,7 @@
             </span>
         </button>
     </div>
+    
     {{-- nur rizki selesain yang ini --}} 
 
-    
 </x-app-layout>
